d002f3190d8aeec5b78de18cc732e5e697aeafb2
joyanhui <leiyanhui@gmail.com>
2025-07-12 11:23:05 +0800

    feat: Add LOG_LEVEL environment variable support
    This commit adds support for setting the log level using the LOG_LEVEL environment variable.
    
    The supported levels are DEBUG, INFO, WARN, ERROR, DPANIC, PANIC, and FATAL. If the environment variable is not set or has an invalid value, the
    default log level is INFO.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # golang-pkgs
 
 ```sh
-### zapLogger
+### zapLogger 日志等级 用 环境变量 DEBUG  INFO ERROR   DPANIC PANIC FATAL
 
 GOPROXY=direct  go get github.com/joyanhui/golang-pkgs/pkgs/zapLogger@main
 
--- pkgs/zapLogger/zap.go
+++ pkgs/zapLogger/zap.go
@@ -3,6 +3,7 @@ package zapLogger
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -20,6 +21,9 @@ func InitZapLogger(isDevMode bool) (*zap.SugaredLogger, error) {
 		config.OutputPaths = []string{"stderr"}
 		config.Encoding = "console" // 输出格式 console 或 json
 		config.ErrorOutputPaths = []string{"stderr"}
+		logLevelStr := os.Getenv("LOG_LEVEL") // 日志登基
+		effectiveLogLevel := parseLogLevel(logLevelStr)
+		config.Level = zap.NewAtomicLevelAt(effectiveLogLevel)
 		if isDevMode {
 			config.DisableCaller = false
 		} else {
@@ -35,4 +39,26 @@ func InitZapLogger(isDevMode bool) (*zap.SugaredLogger, error) {
 		logger = plainLogger.Sugar()
 	}
 	return logger, nil
+}
+
+func parseLogLevel(levelStr string) zapcore.Level {
+	switch strings.ToUpper(levelStr) { // 将输入字符串转换为大写进行匹配
+	case "DEBUG":
+		return zapcore.DebugLevel
+	case "INFO":
+		return zapcore.InfoLevel
+	case "WARN", "WARNING": // 同时支持 "WARN" 和 "WARNING"
+		return zapcore.WarnLevel
+	case "ERROR":
+		return zapcore.ErrorLevel
+	case "DPANIC":
+		return zapcore.DPanicLevel
+	case "PANIC":
+		return zapcore.PanicLevel
+	case "FATAL":
+		return zapcore.FatalLevel
+	default:
+		// 如果环境变量未设置或解析失败，默认日志级别为 Info
+		return zapcore.InfoLevel
+	}
 }
