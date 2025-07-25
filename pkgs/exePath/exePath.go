a41a4614e4ed96a461fa74be610f9895cfa8d86d
joyanhui <leiyanhui@gmail.com>
2025-07-25 16:36:23 +0800

    Refactor: Remove exePath_test.go and update logging in GetExeDir function
    This commit deletes the exePath_test.go file and modifies the GetExeDir function to use fmt.Printf for error logging instead of log.Fatalf, improving error handling without terminating the program.

--- pkgs/exePath/exePath.go
+++ pkgs/exePath/exePath.go
@@ -1,23 +1,22 @@
 package exePath
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"path/filepath"
 )
 
-// 实际是dit
 func GetExeDir(isDev bool) string {
 	if isDev {
 		wd, err := os.Getwd()
 		if err != nil {
-			log.Fatalf("cannot get current working directory: %v", err)
+			fmt.Printf("[GetExeDir] cannot get current working directory: %v \n", err)
 		}
 		return wd
 	} else {
 		executablePath, err := os.Executable()
 		if err != nil {
-			log.Fatalf("cannot get executable path: %v", err)
+			fmt.Printf("[GetExeDir] cannot get executable path: %v \n", err)
 		}
 		return filepath.Dir(executablePath)
 	}
